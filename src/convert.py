721a31e11c7d5b05da13e7af8634d3d788d87f96
erastov-alex <erastov.alex@gmail.com>
2023-10-03 10:27:06 +0300

    upd convert, settings

--- src/convert.py
+++ src/convert.py
@@ -1,10 +1,9 @@
 import supervisely as sly
-import os
+import os, glob
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 
 from tqdm import tqdm
 
@@ -70,16 +69,66 @@ def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
-
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    dataset_path = "COVID-19_Radiography_Dataset"
+    images_folder = "images"
+    masks_folder = "masks"
+    ds_name = "ds"
+    batch_size = 30
 
 
+    def create_ann(image_path):
+        labels = []
+
+        tag_value = image_path.split("/")[-3]
+        tag_meta = meta.get_tag_meta(tag_value)
+        tag = sly.Tag(tag_meta)
+
+        image_name = get_file_name_with_ext(image_path)
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
+
+        mask_path = os.path.join(dataset_path, tag_value, masks_folder, image_name)
+        mask_np = sly.imaging.image.read(mask_path)[:, :, 0]
+        mask = mask_np == 255
+        curr_bitmap = sly.Bitmap(mask)
+        curr_label = sly.Label(curr_bitmap, obj_class)
+        labels.append(curr_label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=[tag])
+
+
+    obj_class = sly.ObjClass("lungs", sly.Bitmap)
+
+
+    tag_covid = sly.TagMeta("COVID", sly.TagValueType.NONE)
+    tag_opacity = sly.TagMeta("Lung_Opacity", sly.TagValueType.NONE)
+    tag_normal = sly.TagMeta("Normal", sly.TagValueType.NONE)
+    tag_pneumonia = sly.TagMeta("Viral Pneumonia", sly.TagValueType.NONE)
+
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[obj_class],
+        tag_metas=[tag_covid, tag_opacity, tag_normal, tag_pneumonia],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    images_pathes = glob.glob(dataset_path + "/*/images/*.png")
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+    for images_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+        img_names_batch = [get_file_name_with_ext(img_path) for img_path in images_pathes_batch]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
